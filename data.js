109aff73992120b587da5f788ef248dbf73c18ae
undertrox <undertrox@gmail.com>
2026-07-25 14:52:17 +0200

    initial commit

---
+++ data.js
@@ -0,0 +1,41 @@
+window.data = {
+    "name": "DB 2026",
+    "matches": [
+        {
+            "num": 0,
+            "team1": "bodo_ori",
+            "team2": "Dariusbugs",
+            "team3": "Masterozo",
+            "score": {"noPenalty": [50, 30, 20], "withPenalty": [40, 29, 20]},
+            "group": "Group A",
+            "start": "2026-07-17 00:00 UTC+2",
+            "end": "2026-08-08 23:59 UTC+2",
+        },
+        {
+            "num": 1,
+            "team1": "anice5293",
+            "team2": "SpeedWagon",
+            "team3": "SilverPaper",
+            "score": {},
+            "group": "Group B",
+            "start": "2026-07-17 00:00 UTC+2",
+            "end": "2026-08-08 23:59 UTC+2",
+        },
+        {
+            "num": 16,
+            "round": 2,
+            "score": {},
+            "date": "2026-08-08",
+            "time": "23:59 UTC+2"
+        }
+    ]
+};
+
+window.abbreviations = {
+    "bodo_ori": "bodo",
+    "Dariusbugs": "Darius",
+    "Masterozo": "Masterozo",
+    "anice5293": "anice",
+    "SpeedWagon": "SpeedWagon",
+    "SilverPaper": "SilverPaper",
+}
